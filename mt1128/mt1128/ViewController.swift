fbc4ea621f0b1e9a79f3abf14dbd60c8c9d8edb0
eensungkim <eensungkim@gmail.com>
2024-06-19 13:49:30 +0900

    config: 초기 세팅

---
+++ mt1128/mt1128/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  mt1128
+//
+//  Created by Kim EenSung on 6/19/24.
+//
+
+import UIKit
+
+final class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemRed
+    }
+
+
+}
+
